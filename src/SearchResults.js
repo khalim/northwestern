e92a0ea36ebbc5f70834c838d336d98c722a9b9e
Khalim Harris <khalimharris@gmail.com>
2020-04-21 23:20:04 -0400

    consolidate code into one function from the 2 hooks: componentDidUpdate and componentDidMount

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -8,6 +8,35 @@ export default class SearchResults extends React.Component {
             searchTerm: "",
             resultNames: []
         }
+
+        this.updateSearchResults = this.updateSearchResults.bind(this);
+    }
+
+    async updateSearchResults() {
+        // get search URL prepared for APPLE iTunes API. --> works over proxy, set in package.json
+        const url = "/search?term=" + this.props.searchTerm + "&entity=" + this.props.mediaType
+        const response = await fetch(url)
+
+        let fullList = []
+        let data = []
+        let tempList = []
+
+        data = await response.json()
+        fullList = data.results
+
+        // Build *just* the list of titles
+        for (const listEntry of fullList) {
+            tempList = tempList.concat([listEntry.trackName])
+        }
+
+console.log(tempList)
+        // return sorted, Alphabetically
+        return tempList.sort()
+
+        //  Set up an error message if iTunes is clueless
+        if (tempList.length == 0) {
+            tempList = tempList.concat("Sorry, iTunes returned no items")
+        }
     }
 
     async componentDidUpdate(prevProps) {
@@ -16,52 +45,13 @@ export default class SearchResults extends React.Component {
             // Reset resultNames to ensure there are no remnants or concatenation to it.
             this.setState({resultNames: []})
 
-            var fullList = []
-            var data = []
-            var tempList = []
-
-            // get search URL prepared for APPLE iTunes API. --> works over proxy
-            const url = "/search?term=" + this.props.searchTerm + "&entity=" + this.props.mediaType
-            const response = await fetch(url)
-
-            data = await response.json()
-            fullList = data.results
-    
-            // Build *just* the list of titles
-            for (const listEntry of fullList) {
-                tempList = tempList.concat([listEntry.trackName])
-            }
-
-            // sort Alphabetically
-            tempList.sort()
-
-            //  Set up an error message if iTunes is clueless
-            if (tempList.length == 0) {
-                tempList = tempList.concat("Sorry, iTunes returned no items")
-            }
-
             // update resultNames with fully sorted list
-            this.setState({resultNames: tempList})
+            this.setState({resultNames: await this.updateSearchResults()})
         }
     }
 
     async componentDidMount() {
-        const url = "/search?term=" + this.props.searchTerm + "&entity=" + this.props.mediaType
-        const response = await fetch(url)
-
-        var fullList = []
-        var data = []
-        var tempList = []
-
-        data = await response.json()
-        fullList = data.results
-
-        for (const listEntry of fullList) {
-            tempList = tempList.concat([listEntry.trackName])
-        }
-
-        tempList.sort()
-        this.setState({resultNames: tempList})
+        this.setState({resultNames: await this.updateSearchResults()})
     }
 
 
